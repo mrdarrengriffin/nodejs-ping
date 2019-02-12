402428f74a3c67520ccb287f35627862fb5c87ce
Darren Griffin <darren.griffin@live.co.uk>
2019-02-12 08:18:33 +0000

    Fixed wrong function instantiation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-module.exports.ping(ip) = {
+module.exports.ping = function(ip){
     return new Promise((resolve, reject) => {
         exec("ping -c 1 " + ip + " -w 1", (res) => {
             if (res == null) {

d77b251a5b3a2ec7f559baabd3efdad3728deb4a
Darren Griffin <darren.griffin@live.co.uk>
2019-02-12 08:04:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const exec = require('child_process').exec;
+
+module.exports.ping(ip) {
+    return new Promise((resolve, reject) => {
+        exec("ping -c 1 " + ip + " -w 1", (res) => {
+            if (res == null) {
+                resolve({ host: ip, result: true })
+            } else {
+                reject({ host: ip, result: false })
+            }
+        })
+    });
+}
